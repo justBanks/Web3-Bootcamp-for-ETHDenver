f3b4264c371420b1f98a90aebe95fde7b2f88f1e
Darryl Banks <277740+justBanks@users.noreply.github.com>
2022-11-15 03:18:42 -0800

    homework 11: make VolcanoNFT payable

--- VolcanoCoin/contracts/VolcanoNFT.sol
+++ VolcanoCoin/contracts/VolcanoNFT.sol
@@ -11,34 +11,39 @@ contract VolcanoNFT is Ownable, ERC721URIStorage
 {
     using Counters for Counters.Counter; // gives _tokenIds extension methods from Counters library
     Counters.Counter private _tokenIds;
+    string private baseURI;
     uint private _totalSupply = 17;
-    mapping(address => uint) _owners;
+    mapping(address => uint[]) public _tokensOwned;
 
-    constructor() Ownable() ERC721("Pompay", "POMP") {
+    constructor() payable Ownable() ERC721("Pompay", "POMP") {
         safeMint(msg.sender);
         safeMint(msg.sender);
         safeMint(msg.sender); // spare token, to transfer with team wallet
     }
 
     // tokenURI should resolve to a metadata JSON schema
-    function safeMint(address to) private {
+    function safeMint(address to) private returns(uint) {
         _tokenIds.increment();
         require(_tokenIds.current() < _totalSupply, "Total supply of tokens has been claimed");
-        uint256 newItemId = _tokenIds.current();
-        _safeMint(to, newItemId);
+        _safeMint(to, _tokenIds.current());
         //_setTokenURI(newItemId, "{\"name\": \"POMP token\", \"tokenId\": }");
         _totalSupply--;
-        _owners[to] = newItemId;
+        _tokensOwned[to].push(_tokenIds.current());
         console.log("Total supply remaining: ", _totalSupply);
+        return _tokenIds.current();
     }
 
-    function mintToken() public {
-        require(msg.sender == owner() || _owners[msg.sender] == 0, "You already own a token");
-        safeMint(msg.sender);
+    /** 
+     * Why pass in a token URI here, when ERC721 provides a standardized tokenURI() function? 
+     */
+    function mintToken() external payable returns(uint) {
+        require(_tokenIds.current() < _totalSupply, "Total supply of tokens has been claimed");
+        require(msg.value == 10000000000000000, "First gimme .01 GoETH");
+        return safeMint(msg.sender);
     }
 
-    function whichTokenDoIOwn() public view returns (uint) {
-        return _owners[msg.sender];
+    function whichTokensDoIOwn() public view returns (uint[] memory) {
+        return _tokensOwned[msg.sender];
     }
 
     function transfer(address to, uint tokenId) public {
@@ -51,6 +56,19 @@ contract VolcanoNFT is Ownable, ERC721URIStorage
 
     function totalSupplyRemaining() public view returns (uint) {
         return _totalSupply;
+    }
+
+    /**
+     * @dev Base URI for computing {tokenURI}. If set, the resulting URI for each
+     * token will be the concatenation of the `baseURI` and the `tokenId`.
+     * Just call tokenURI(tokenId).
+     */
+    function _baseURI() internal view override returns (string memory) {
+        return baseURI;
+    }
+
+    function setBaseURI(string memory uri) public onlyOwner {
+        baseURI = uri;
     }
     
 }
